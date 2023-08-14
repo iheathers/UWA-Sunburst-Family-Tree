0222efd7b053433edb32afdaec92697e16a2a58c
Pritam Suwal Shrestha <pshre@Pritams-Air.lan>
2023-08-14 11:56:16 +0800

    Test precommit hook

---
+++ backend/src/models/Bio.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const BioSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model('Posts', BioSchema);

12a24fe0723af249b5705d87373ae5e56e5faa30
Pritam Suwal Shrestha <pshre@Pritams-Air.lan>
2023-08-20 23:59:41 +0800

    Update eslint config

--- backend/src/controllers/Bio.controller.js
+++ backend/src/controllers/Bio.controller.js
@@ -1,19 +1,19 @@
-const Post = require('../models/post');
+const Post = require('../models/post')
 
 const getPosts = async (req, res, next) => {
   try {
-    const posts = await Post.find();
+    const posts = await Post.find()
 
     if (!posts.length) {
-      throw new Error('Could not fetch posts');
+      throw new Error('Could not fetch posts')
     }
 
     res.status(200).json({
-      posts,
-    });
+      posts
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
-module.exports = { getPosts };
+module.exports = { getPosts }
--- backend/src/models/Bio.model.js
+++ backend/src/models/Bio.model.js
@@ -1,25 +1,25 @@
 // TEMPLATE
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const BioSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      required: true
     },
     location: {
       type: String,
-      required: true,
+      required: true
     },
     imageUrl: {
       type: String,
-      required: true,
-    },
+      required: true
+    }
   },
   {
-    timestamps: true,
-  },
-);
+    timestamps: true
+  }
+)
 
-module.exports = mongoose.model('Posts', BioSchema);
+module.exports = mongoose.model('Posts', BioSchema)

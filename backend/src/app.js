f7297122ccd2ea81c00dd6fa821085caa91a575c
Pritam Suwal Shrestha <pshre@Pritams-Air.lan>
2023-08-21 10:20:16 +0800

    Test

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,30 +12,30 @@
 //   console.log(`Example app listening on port ${port}`)
 // })
 
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri =
-  'mongodb+srv://test1:Nm8ndGA6y323fWX@uwa-sunburst-server.lhgpneg.mongodb.net/?retryWrites=true&w=majority'
+  'mongodb+srv://test1:Nm8ndGA6y323fWX@uwa-sunburst-server.lhgpneg.mongodb.net/?retryWrites=true&w=majority';
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
-    deprecationErrors: true
-  }
-})
+    deprecationErrors: true,
+  },
+});
 
-async function run () {
+async function run() {
   try {
-    await client.connect()
+    await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
+    await client.db('admin').command({ ping: 1 });
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
+      'Pinged your deployment. You successfully connected to MongoDB!',
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close()
+    await client.close();
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);

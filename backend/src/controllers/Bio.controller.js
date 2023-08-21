a9d3c82d25d6a89cd6191a4214f81703879fbaa5
Pritam Suwal Shrestha <pshre@Pritams-Air.lan>
2023-08-21 11:07:22 +0800

    Add project structure

---
+++ backend/src/controllers/Bio.controller.js
@@ -0,0 +1,19 @@
+const Post = require('../models/post');
+
+const getPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find();
+
+    if (!posts.length) {
+      throw new Error('Could not fetch posts');
+    }
+
+    res.status(200).json({
+      posts,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getPosts };

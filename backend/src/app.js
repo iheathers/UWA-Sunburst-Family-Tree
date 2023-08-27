b99614b70facb9eb7cefe330337cde6eb2348595
kuogao <23318508@student.uwa.edu.au>
2023-08-28 05:03:07 +0800

    link mongodb and create four collection

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,23 +2,26 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import mongoose from "mongoose";
-
+import mongoose from "mongoose"
 import familyMemberRouter from "./routes/FamilyMember.route.js";
 
 const app = express();
-
 app.use(express.json());
+console.log(process.env.MONGODB_URI)
 
 app.use("/api/family-member", familyMemberRouter);
-
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(
+// connect to mongodb
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log("App listening on port 8080");
-    })
-  )
+      console.log(`App listening on port ${process.env.PORT}`);
+    });
+  })
   .catch((err) => {
-    console.log({ err });
+    console.error('Mongoose connection error:', err);
   });
+
+
+
+
+
--- backend/src/models/FamilyMember.model.js
+++ backend/src/models/FamilyMember.model.js
@@ -1,9 +1,43 @@
 import mongoose from "mongoose";
 
 const familyMemberSchema = new mongoose.Schema({
+  id: { type: Number, required: true, unique: true },
   name: { type: String, required: true },
 });
 
-const FamilyMember = mongoose.model("FamilyMember", familyMemberSchema);
+const detailSchema = new mongoose.Schema({
+  id: { type: Number, required: true, ref: "FamilyMember" }, // Change to 'FamilyMember'
+  birthOfDay: { type: Date },
+  deathOfDay: { type: Date },
+  address: [{ type: String }],
+  occupation: { type: String },
+  phoneNumber: { type: String },
+  email: { type: String },
+});
 
-export default FamilyMember;
+const commentSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "FamilyMember", // Change to 'FamilyMember'
+    required: true,
+  },
+  date: { type: Date, default: Date.now },
+  content: { type: String, required: true },
+});
+
+const adviseSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "FamilyMember", // Change to 'FamilyMember'
+    required: true,
+  },
+  date: { type: Date, default: Date.now },
+  content: { type: String, required: true },
+});
+
+const FamilyMember = mongoose.model("FamilyMember", familyMemberSchema);
+const Advise = mongoose.model("Advise", adviseSchema);
+const Comment = mongoose.model("Comment", commentSchema);
+const Detail = mongoose.model("Detail", detailSchema);
+
+export default { FamilyMember, Comment, Advise, Detail };

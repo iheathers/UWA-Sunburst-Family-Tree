52e70c287aacc29df05becfde3467029a1defb7e
Pritam Suwal Shrestha <pshre@Pritams-Air.lan>
2023-09-01 19:56:45 +0800

    Revert "link mongodb and create four collection"
    This reverts commit b99614b70facb9eb7cefe330337cde6eb2348595.

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,26 +2,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+
 import familyMemberRouter from "./routes/FamilyMember.route.js";
 
 const app = express();
+
 app.use(express.json());
-console.log(process.env.MONGODB_URI)
 
 app.use("/api/family-member", familyMemberRouter);
-// connect to mongodb
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(
     app.listen(process.env.PORT, () => {
-      console.log(`App listening on port ${process.env.PORT}`);
-    });
-  })
+      console.log("App listening on port 8080");
+    })
+  )
   .catch((err) => {
-    console.error('Mongoose connection error:', err);
+    console.log({ err });
   });
-
-
-
-
-

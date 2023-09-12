621d271d3d9e9ec1334290d5e66ca35e7dbf98e4
Pritam Suwal Shrestha <pshre@Pritams-Air.lan>
2023-09-12 13:05:17 +0800

    Experiment different approach of sunburst chart with anychart

--- frontend/src/app/(family)/family-tree/page.js
+++ frontend/src/app/(family)/family-tree/page.js
@@ -1,9 +1,59 @@
+"use client";
+
+// import SunburstChartHookTest from "@/components/SunburstChart/SunburstChartHookTest";
+import SunburstChartTest1 from "@/components/SunburstChart/SunburstChartTest1";
+// import SunburstConfigChart from "@/components/SunburstChart/SunburstChartConfig";
+
+const data = [
+  {
+    name: "Company A",
+    children: [
+      {
+        name: "Technical",
+        children: [
+          { name: "Team Leaders" },
+          { name: "Architects" },
+          { name: "Developers" },
+          {
+            name: "Testers",
+            children: [{ name: "Automation" }, { name: "Manual" }],
+          },
+        ],
+      },
+      {
+        name: "Sales",
+        children: [
+          { name: "Analysts" },
+          {
+            name: "Executives",
+            children: [
+              {
+                name: "Technical",
+                children: [
+                  { name: "Team Leaders" },
+                  { name: "Architects" },
+                  { name: "Developers" },
+                  { name: "Testers" },
+                ],
+              },
+              {
+                name: "Sales",
+                children: [{ name: "Analysts" }, { name: "Executives" }],
+              },
+              { name: "HR" },
+              { name: "Management" },
+            ],
+          },
+        ],
+      },
+      { name: "HR" },
+      { name: "Management" },
+    ],
+  },
+];
+
 const FamilyTreePage = () => {
-  return (
-    <div>
-      <h1>Family Tree</h1>
-    </div>
-  );
+  return <SunburstChartTest1 data={data} />;
 };
 
 export default FamilyTreePage;
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,3 +1,5 @@
+//TODO: redirect to login page if user is not logged in
+
 export default function Home() {
   return <main>Home</main>;
 }

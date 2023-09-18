1dadd8ea7871806083725fb00f3920165fb9655e
trieuH <115166533+trieuH@users.noreply.github.com>
2023-09-18 22:24:49 +0800

    Change formatting and error messages

--- backend/src/controllers/User.controller.js
+++ backend/src/controllers/User.controller.js
@@ -5,82 +5,82 @@ import { validationResult } from "express-validator";
 import User from "../models/User.model.js";
 
 export const signUpUser = async (req, res, next) => {
-    const validationErrors = validationResult(req);
+  const validationErrors = validationResult(req);
 
-    if (!validationErrors.isEmpty()) {
-        const errorsArray = validationErrors.array();
-        const errObj = {
-            message: "Validation Error",
-            error: errorsArray,
-        };
+  if (!validationErrors.isEmpty()) {
+    const errorsArray = validationErrors.array();
+    const errObj = {
+      error: "Validation Error",
+      error: errorsArray,
+    };
 
-        return res.status(422).json(errObj);
+    return res.status(422).json(errObj);
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({
+      email: email,
+    });
+
+    if (existingUser) {
+      return res.status(422).json({
+        error: "User exists",
+      });
     }
 
-    const { email, password } = req.body;
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    try {
-        const existingUser = await User.findOne({ 
-            email: email,
-         });
+    const newUser = new User({
+      email: email,
+      password: hashedPassword,
+    });
 
-        if (existingUser) {
-            return res.status(422).json({
-                message: "User exists",
-            });
-        }
-        
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
+    await newUser.save();
 
-        const newUser = new User({
-            email: email,
-            password: hashedPassword,
-        });
-
-        await newUser.save();
-
-        res.status(201).json({
-            message: "User Created",
-        });
-    } catch (error) {
-        next(error);
-    }
+    res.status(201).json({
+      message: "User Created",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const loginUser = async (req, res, next) => {
-    const { email, password } = req.body;
-    
-    try {
-        const user = await User.findOne({ email: email });
+  const { email, password } = req.body;
 
-        if (!user) {
-            return res.status(401).json({ 
-                error: "User does not exist",
-            });
-        }
+  try {
+    const user = await User.findOne({ email: email });
 
-        const matches = await bcrypt.compare (password, user.password);
-
-        if (!matches) {
-            return res.status(401).json({ 
-                error: "Incorrect password",
-             });
-        }
-
-        // JWT to be implemented
-
-        res.status(200).json({
-            userId: user._id.toString(),
-            accessPermissions: user.accessPermissions
-        });
-    } catch (error) {
-        next(error);
+    if (!user) {
+      return res.status(401).json({
+        error: "Incorrect email or password",
+      });
     }
+
+    const matches = await bcrypt.compare(password, user.password);
+
+    if (!matches) {
+      return res.status(401).json({
+        error: "Incorrect email or password",
+      });
+    }
+
+    // JWT to be implemented
+
+    res.status(200).json({
+      userId: user._id.toString(),
+      accessPermissions: user.accessPermissions,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const listUsers = async (req, res, next) => {
-    /*
+  /*
     Fetch users from database
         User.find()
     If users found:
@@ -89,10 +89,10 @@ export const listUsers = async (req, res, next) => {
         Return response indicating no users found
     Catch errors
     */
-}
+};
 
 export const changeUserPermissions = async (req, res, next) => {
-    /*
+  /*
     Get userId and permission from request
     Find user - findById()
         If user not found, return response indicating cannot find user (404)
@@ -106,7 +106,7 @@ export const changeUserPermissions = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-    /*
+  /*
     Find user and delete - findByIdAndDelete()
     If user not found, return response indicating cannot find user (404)
     Return successful response (204)
--- backend/src/routes/User.route.js
+++ backend/src/routes/User.route.js
@@ -1,20 +1,14 @@
 import express from "express";
 import { body } from "express-validator";
 
-import {
-    signUpUser, 
-    loginUser,
-} from "../controllers/User.controller.js";
+import { signUpUser, loginUser } from "../controllers/User.controller.js";
 
 const userRouter = express.Router();
 
 userRouter.post(
-    "/signup",
-    [
-        body("email").isEmail(),
-        body("password").not().isEmpty(),
-    ],
-    signUpUser
+  "/signup",
+  [body("email").isEmail(), body("password").not().isEmpty()],
+  signUpUser
 );
 
 userRouter.post("/login", loginUser);

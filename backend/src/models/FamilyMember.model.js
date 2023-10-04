a0f6b729339f697d95f3eac4dc6a94a6f9023913
trieuH <115166533+trieuH@users.noreply.github.com>
2023-10-04 19:38:12 +0800

    Revert changes to family tree data field id

--- backend/src/models/FamilyMember.model.js
+++ backend/src/models/FamilyMember.model.js
@@ -1,53 +1,40 @@
 import mongoose from "mongoose";
 
-const familyMemberSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    parent: {
+const familyMemberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  parent: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "FamilyMember",
+  },
+  children: [
+    {
       type: mongoose.Schema.Types.ObjectId,
       ref: "FamilyMember",
     },
-    children: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "FamilyMember",
-      },
-    ],
-    birthDate: {
-      type: Date,
-    },
-    deathDate: {
-      type: Date,
-    },
-    location: {
-      type: String,
-    },
-    occupation: {
-      type: String,
-    },
-    about: {
-      type: String,
-    },
-    displayOnChart: {
-      type: Boolean,
-      default: true,
-    },
+  ],
+  birthDate: {
+    type: Date,
   },
-  {
-    // Define schema options
-    versionKey: false, // Exclude the '__v' field
-    toJSON: {
-      virtuals: true,
-      transform: (doc, ret) => {
-        ret.id = ret._id; // Use 'id' instead of '_id' in the JSON output
-        delete ret._id; // Remove '_id' from the JSON output
-      },
-    },
-  }
-);
+  deathDate: {
+    type: Date,
+  },
+  location: {
+    type: String,
+  },
+  occupation: {
+    type: String,
+  },
+  about: {
+    type: String,
+  },
+  displayOnChart: {
+    type: Boolean,
+    default: true,
+  },
+});
 
 const FamilyMember = mongoose.model("FamilyMember", familyMemberSchema);
 

a3666395f9245afab3d06d4e64fc2d253aed4e29
Pritam Suwal Shrestha <shresthapritam7@gmail.com>
2023-10-09 21:57:30 +0800

    Remove comments

--- frontend/src/app/(family)/family-tree/page.js
+++ frontend/src/app/(family)/family-tree/page.js
@@ -1,15 +1,14 @@
 "use client";
 
-// Testing
-
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import data from "@/data/familyTree/farsiSimpleFamilyTree.json";
+
+import Link from "next/link";
+
+import React, { useState, useEffect } from "react";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 import SunburstChart from "@/components/SunburstChart/SunburstChart";
 import ZoomController from "@/components/ZoomController/ZoomController";
-import Link from "next/link";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_ENDPOINT_BASE_URL;
 const familyTreeRoute = process.env.NEXT_PUBLIC_FAMILY_TREE_ROUTE;
@@ -36,15 +35,6 @@ const FamilyTreePage = () => {
     fetchData();
   }, [apiUrl, familyTreeRoute]);
 
-  const addRootNode = () => {
-    console.log("Add Root Node");
-
-    // router.push("family-member/add");
-
-    // as the unique ID for the root node
-    // router.push("/edit-family-member/[id]", `/edit-family-member/root`);
-  };
-
   return (
     <>
       <TransformWrapper>
@@ -63,7 +53,6 @@ const FamilyTreePage = () => {
                 <Link
                   href={{
                     pathname: "/family-member/root/add",
-                    // query: { id: null },
                   }}
                 >
                   Add Root Node

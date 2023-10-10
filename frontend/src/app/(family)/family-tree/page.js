5ed121af238f2b861c498d2b1d63366ccc99902b
Yuting Ma <23796026@student.uwa.edu.au>
2023-10-10 10:25:48 +0800

    Add the the logout function Remove the jwtToken after logout

--- frontend/src/app/(family)/family-tree/page.js
+++ frontend/src/app/(family)/family-tree/page.js
@@ -3,6 +3,7 @@
 // Testing
 
 import React from "react";
+import { useRouter } from 'next/navigation';
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 import data from "@/data/familyTree/farsiSimpleFamilyTree.json";
@@ -11,8 +12,16 @@ import SunburstChart from "@/components/SunburstChart/SunburstChart";
 import ZoomController from "@/components/ZoomController/ZoomController";
 
 const FamilyTreePage = () => {
+  const router = useRouter();
+
+  const logout = () => {
+    localStorage.removeItem("jwtToken");
+    router.push("/login");
+  };
+
   return (
     <>
+      <button onClick={logout}>Logout</button>
       <TransformWrapper>
         <TransformComponent>
           {/* TODO: IF API CALL RERENDER EVERY COMPONENT, EXTRACT THE CHART IN SEPARATE FILE */}
